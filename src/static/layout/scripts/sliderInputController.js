ae65a68caa85a0c73c255ed92d54909068df4299
Sameh Frihat <samehfrihat@gmail.com>
2022-05-11 16:43:45 +0200

    fix slider

--- src/static/layout/scripts/sliderInputController.js
+++ src/static/layout/scripts/sliderInputController.js
@@ -1,14 +1,25 @@
 let volumeSlider = document.getElementById('level_en');
 let volumeSlider_de = document.getElementById('level_de');
 
+let readabilityVolumeSlider_de = document.getElementById('readability_level_de');
+let readabilityVolumeSlider = document.getElementById('read_level_en');
+
+
+
+
 function setEnglishAbstractTechnicalityLevel(wert) {
     document.getElementById('output_en').value = wert;
 }
-
 function setGermanAbstractTechnicalityLevel(wert) {
     document.getElementById('output_de').value = wert;
 }
 
+function setGermanAbstractReadabilityLevel(wert) {
+    document.getElementById('readability_output_de').value = wert;
+}
+function setEnglishAbstractReadabilityLevel(wert) {
+    document.getElementById('readability_output_en').value = wert;
+}
 
 window.onload = function () {
     if (volumeSlider != null) {
@@ -24,4 +35,19 @@ window.onload = function () {
         });
         setGermanAbstractTechnicalityLevel(volumeSlider_de.value);
     }
+    if (readabilityVolumeSlider_de != null) {
+        readabilityVolumeSlider_de.addEventListener('input', function (event) {
+            setGermanAbstractReadabilityLevel(event.target.value);
+        });
+        setGermanAbstractReadabilityLevel(readabilityVolumeSlider_de.value);
+    }
+
+    if (readabilityVolumeSlider != null) {
+        readabilityVolumeSlider.addEventListener('input', function (event) {
+            setEnglishAbstractReadabilityLevel(event.target.value);
+        });
+        setEnglishAbstractReadabilityLevel(readabilityVolumeSlider.value);
+    }
+
+
 }
