fa8fcd5010f5eadd9a654be45ae2a6a859d22377
Sameh Frihat <samehfrihat@gmail.com>
2022-11-11 16:32:52 +0100

    fix translation

--- src/run.py
+++ src/run.py
@@ -1,6 +1,12 @@
 from src import app
 from src.config import PORT, DEBUG
 
+#from babel.messages.frontend import CommandLineInterface
+
+#CommandLineInterface().run(['pybabel','extract','-F','babel.cfg','-k','lazy_gettext','-o','messages.pot','--input-dirs=.'])
+#CommandLineInterface().run(['pybabel','update','-i','messages.pot','-d','./translations','-l','de'])
+#CommandLineInterface().run(['pybabel','compile','-d','translations'])
+
 if __name__ == "__main__":
     print("Start server ....")
     if DEBUG:
